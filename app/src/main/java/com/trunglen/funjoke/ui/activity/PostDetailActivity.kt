c5f08f7500b86cc4139964e8082d5817df19a12e
trunglen <trunglenlvn@gmail.com>
2018-05-03 17:52:18 +0700

    as

---
+++ app/src/main/java/com/trunglen/funjoke/ui/activity/PostDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.trunglen.funjoke.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_detail)
+    }
+}
